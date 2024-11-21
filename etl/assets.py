94c43b45b2842a97b5f65547063882b965713015
Brian Kariuki <briankariukidev@gmail.com>
2024-11-21 06:54:17 +0300

    create dbt targets on app start

--- etl/assets.py
+++ etl/assets.py
@@ -1,12 +1,19 @@
-from dagster import AssetExecutionContext, Config
+from dagster import AssetExecutionContext, Config, file_relative_path
 from dagster_dbt import DbtCliResource, dbt_assets
 from project import dbt_project
 
 class DbtConfig(Config):
     full_refresh: bool
 
+DBT_PROJECT_DIR = file_relative_path(__file__, "./mdharura_dbt")
 
-@dbt_assets(manifest=dbt_project.manifest_path)
+dbt_resource = DbtCliResource(project_dir=DBT_PROJECT_DIR)
+dbt_parse_invocation = dbt_resource.cli(["parse"]).wait()
+dbt_manifest_path = dbt_parse_invocation.target_path.joinpath("manifest.json")
+
+
+# @dbt_assets(manifest=dbt_project.manifest_path)
+@dbt_assets(manifest=dbt_manifest_path)
 def mdharura_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource, config: DbtConfig):
     dbt_build_args = ["build"]
     if config.full_refresh:
