d50d1b1850817e3673a65a5fd804cd0b9905dd4f
Brian Kariuki <briankariukidev@gmail.com>
2025-01-23 11:10:02 +0300

    fct tables for units created manually and not in official units list

--- etl/mdharura_dbt/models/data_mart/fct_community_units.sql
+++ etl/mdharura_dbt/models/data_mart/fct_community_units.sql
@@ -1,4 +1,32 @@
--- TODO: Cater for unset community units with valid subcounties. i.e community units created manually from mdharura app
+WITH recursive units_hierarchy AS (
+  -- Base case: community unit matches the official community unit
+  SELECT
+    units.*,
+    community_units.county_key,
+    community_units.sub_county_key,
+    community_units.community_unit_key
+  FROM
+    {{ ref('stg_units') }} AS units
+    LEFT JOIN {{ ref('dim_community_units') }} AS community_units
+    ON community_units.uid = units."UID"
+  WHERE
+    units."TYPE" = 'Community unit'
+    AND units."UID" IS NOT NULL
+  UNION ALL
+    -- Recursive case: community unit does not match the official community unit
+  SELECT
+    m_units.*,
+    subcounty_units."COUNTY_KEY" AS county_key,
+    subcounty_units."SUB_COUNTY_KEY" AS sub_county_key,
+    'unset' AS community_unit_key
+  FROM
+    {{ ref('stg_units') }} AS m_units
+    LEFT JOIN {{ ref('fct_sub_county_units') }} AS subcounty_units
+    ON subcounty_units."_ID" = m_units."PARENT"
+  WHERE
+    m_units."TYPE" = 'Community unit'
+    AND m_units."UID" IS NULL
+)
 SELECT
   COALESCE(
     community_units.county_key,
@@ -18,7 +46,7 @@ SELECT
   ) AS "LOAD_DATE"
 FROM
   {{ ref('stg_units') }} AS units
-  LEFT JOIN {{ ref('dim_community_units') }} AS community_units
-  ON community_units.uid = units."UID"
+  LEFT JOIN units_hierarchy AS community_units
+  ON community_units."_ID" = units."_ID"
 WHERE
   units."TYPE" = 'Community unit'
--- etl/mdharura_dbt/models/data_mart/fct_health_facilities.sql
+++ etl/mdharura_dbt/models/data_mart/fct_health_facilities.sql
@@ -1,4 +1,32 @@
--- TODO: Cater for unset community units with valid subcounties. i.e community units created manually from mdharura app
+WITH recursive units_hierarchy AS (
+  -- Base case: community unit matches the official community unit
+  SELECT
+    units.*,
+    health_facilities.county_key,
+    health_facilities.sub_county_key,
+    health_facilities.health_facility_key
+  FROM
+    {{ ref('stg_units') }} AS units
+    LEFT JOIN {{ ref('dim_health_facilities') }} AS health_facilities
+    ON health_facilities.uid = units."UID"
+  WHERE
+    units."TYPE" = 'Health facility'
+    AND units."UID" IS NOT NULL
+  UNION ALL
+    -- Recursive case: community unit does not match the official community unit
+  SELECT
+    m_units.*,
+    subcounty_units."COUNTY_KEY" AS county_key,
+    subcounty_units."SUB_COUNTY_KEY" AS sub_county_key,
+    'unset' AS health_facility_key
+  FROM
+    {{ ref('stg_units') }} AS m_units
+    LEFT JOIN {{ ref('fct_sub_county_units') }} AS subcounty_units
+    ON subcounty_units."_ID" = m_units."PARENT"
+  WHERE
+    m_units."TYPE" = 'Health facility'
+    AND m_units."UID" IS NULL
+)
 SELECT
   COALESCE(
     health_facilities.county_key,
@@ -18,7 +46,7 @@ SELECT
   ) AS "LOAD_DATE"
 FROM
   {{ ref('stg_units') }} AS units
-  LEFT JOIN {{ ref('dim_health_facilities') }} AS health_facilities
-  ON health_facilities.uid = units."UID"
+  LEFT JOIN units_hierarchy AS health_facilities
+  ON health_facilities."_ID" = units."_ID"
 WHERE
   units."TYPE" = 'Health facility'
